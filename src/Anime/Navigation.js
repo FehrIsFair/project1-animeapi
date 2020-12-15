615e8e4fef5d033250f7850d23732bd417be703e
VentusAlpha <roxasbeta@gmail.com>
2020-12-14 22:48:40 -0700

    Matching the react comp timeout with the animations

--- src/Anime/Navigation.js
+++ src/Anime/Navigation.js
@@ -61,7 +61,7 @@ const Navigation = () => {
   return (
     <CSSTransition
       in={authContext.isAuthenticated}
-      timeout={200}
+      timeout={1000}
       classNames="slide"
     >
       <NavContent isAuth={authContext.isAuthenticated} />
